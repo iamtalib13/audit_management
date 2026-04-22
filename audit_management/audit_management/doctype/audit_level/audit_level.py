491ef0689a72cc68bbee34ebc04438614cbebec6
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-04-22 17:52:26 +0530

    feat(controller): implement branch_query and get_user_division logic

--- audit_management/audit_management/doctype/audit_level/audit_level.py
+++ audit_management/audit_management/doctype/audit_level/audit_level.py
@@ -6,6 +6,10 @@ from frappe.model.document import Document
 from audit_management.audit_management.utils import is_new_system_enabled
 
 class AuditLevel(Document):
+    def before_insert(self):
+        if not self.division:
+            self.division = get_user_division()
+
     def validate(self):
         if is_new_system_enabled():
             self.remove_blank_rows()
@@ -28,3 +32,35 @@ def fetch_employee(employee_id):
     
     # Return as a list to maintain compatibility with the JS callback expectations
     return [employee] if employee else []
+
+@frappe.whitelist()
+def get_user_division():
+    """Fetch division of the current logged-in user from Employee doctype safely."""
+    # Based on inspection, the field name is 'custom_division'
+    if not frappe.db.has_column("Employee", "custom_division"):
+        # If custom_division field is missing, try to get department as a fallback
+        return frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "department")
+    
+    employee = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "custom_division")
+    return employee
+
+@frappe.whitelist()
+def branch_query(doctype, txt, searchfield, start, page_len, filters):
+    """Query to display sol_id and branch in Link field."""
+    return frappe.db.sql(f"""
+        SELECT 
+            name, 
+            CONCAT(sol_id, '-', branch) as description
+        FROM 
+            `tabSahayog Branch`
+        WHERE 
+            (name LIKE %(txt)s OR sol_id LIKE %(txt)s OR branch LIKE %(txt)s)
+        ORDER BY 
+            name ASC
+        LIMIT 
+            %(start)s, %(page_len)s
+    """, {
+        "txt": f"%{txt}%",
+        "start": start,
+        "page_len": page_len
+    })
