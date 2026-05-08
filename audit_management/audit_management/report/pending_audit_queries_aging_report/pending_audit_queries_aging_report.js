e81cf7a6f108590e2c194632a70c7f88c2635992
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-05-08 15:46:35 +0530

    feat: add advanced filters to Pending Audit Queries Aging Report

--- audit_management/audit_management/report/pending_audit_queries_aging_report/pending_audit_queries_aging_report.js
+++ audit_management/audit_management/report/pending_audit_queries_aging_report/pending_audit_queries_aging_report.js
@@ -1,8 +1,18 @@
-# Copyright (c) 2026, Sahayog and contributors
-# For license information, please see license.txt
+// Copyright (c) 2026, Sahayog and contributors
+// For license information, please see license.txt
 
-frappe.query_reports["Pending Audit Queries & Aging Report"] = {
+frappe.query_reports["Pending Audit Queries Aging Report"] = {
 	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date"
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date"
+		},
 		{
 			"fieldname": "department_alignment",
 			"label": __("Department"),
@@ -14,6 +24,18 @@ frappe.query_reports["Pending Audit Queries & Aging Report"] = {
 			"label": __("Primary Category"),
 			"fieldtype": "Link",
 			"options": "Audit Primary Nature"
+		},
+		{
+			"fieldname": "current_escalation_level",
+			"label": __("Escalation Level"),
+			"fieldtype": "Select",
+			"options": "\nLevel 1\nLevel 2\nLevel 3"
+		},
+		{
+			"fieldname": "emp_division",
+			"label": __("Division"),
+			"fieldtype": "Link",
+			"options": "Division"
 		}
 	]
 };
