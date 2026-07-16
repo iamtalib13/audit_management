40a25f9355c4f0edd09c89b2733e0e4acd739737
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-07-16 13:14:07 +0530

    feat: ARC query type auto TAT config setup
    - audit_tat_config: stage field non-mandatory for blank fallback
    - audit_query_type: auto-create first row with tat_days=15 for ARC

--- audit_management/audit_management/doctype/audit_query_type/audit_query_type.js
+++ audit_management/audit_management/doctype/audit_query_type/audit_query_type.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2026, Sahayog and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Audit Query Type", {
-// 	refresh(frm) {
+frappe.ui.form.on("Audit Query Type", {
+  query_type: function (frm) {
+    if (frm.doc.query_type === "Audit Report Compliance") {
+      if (!frm.doc.tat_config || frm.doc.tat_config.length === 0) {
+        let row = frappe.model.add_child(frm.doc, "Audit TAT Config", "tat_config");
+        row.tat_days = 15;
+        frm.refresh_field("tat_config");
+      } else if (frm.doc.tat_config[0].tat_days !== 15) {
+        frm.doc.tat_config[0].tat_days = 15;
+        frm.refresh_field("tat_config");
+      }
+    }
+  },
 
-// 	},
-// });
+  validate: function (frm) {
+    if (frm.doc.query_type === "Audit Report Compliance") {
+      if (!frm.doc.tat_config || frm.doc.tat_config.length === 0) {
+        let row = frappe.model.add_child(frm.doc, "Audit TAT Config", "tat_config");
+        row.tat_days = 15;
+        frm.refresh_field("tat_config");
+      } else if (frm.doc.tat_config[0].tat_days !== 15) {
+        frm.doc.tat_config[0].tat_days = 15;
+        frm.refresh_field("tat_config");
+      }
+    }
+  }
+});
