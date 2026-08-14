db94c2a12d87d694f6ccc74569f1c2aae047ba53
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-08-14 12:21:02 +0530

    refactor: remove redundant root-level doctype directory

--- audit_management/doctype/djp_case/__init__.py
+++
@@ -1,9 +0,0 @@
-from .djp_case import (
-    get_cmg_mapping,
-    populate_djp_stages,
-    send_to_current_stage,
-    escalate_case,
-    close_case,
-    check_tat_breaches,
-    check_governance_rules
-)
