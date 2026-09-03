934dd1d6af06bd760286885531f66b047dd8f6ae
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-09-03 15:39:32 +0530

    refactor(settings): update enable_dgp_module and dgp_stages fields in Audit Management Settings

--- audit_management/audit_management/doctype/audit_management_settings/audit_management_settings.js
+++ audit_management/audit_management/doctype/audit_management_settings/audit_management_settings.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Audit Management Settings', {
 	refresh: function(frm) {
 		if (frappe.session.user !== 'Administrator') {
-			frm.set_df_property('enable_djp_module', 'read_only', 1);
+			frm.set_df_property('enable_dgp_module', 'read_only', 1);
 		}
 	}
 });
--- audit_management/audit_management/doctype/audit_management_settings/audit_management_settings.py
+++ audit_management/audit_management/doctype/audit_management_settings/audit_management_settings.py
@@ -4,7 +4,7 @@ from frappe.model.document import Document
 
 class AuditManagementSettings(Document):
 	def validate(self):
-		if self.has_value_changed("enable_djp_module"):
+		if self.has_value_changed("enable_dgp_module"):
 			if frappe.session.user != "Administrator":
-				frappe.throw(_("Only Administrator can enable or disable the DJP Module."))
+				frappe.throw(_("Only Administrator can enable or disable the DGP Module."))
 
