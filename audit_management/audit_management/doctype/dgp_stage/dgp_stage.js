fb750eba5f2cce229ec78133d281e337314a0260
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-09-03 15:38:35 +0530

    refactor(doctype): update schema and controller for DGP Stage

--- audit_management/audit_management/doctype/dgp_stage/dgp_stage.js
+++ audit_management/audit_management/doctype/dgp_stage/dgp_stage.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2026, Sahayog and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("DJP Stage", {
+// frappe.ui.form.on("DGP Stage", {
 // 	refresh(frm) {
 
 // 	},
